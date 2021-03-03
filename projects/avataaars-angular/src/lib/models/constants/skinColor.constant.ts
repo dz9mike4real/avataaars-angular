555a85bf54a3407e325267b88e2d93c25a06eb44
Ashish Kumar <sayhitoak@gmail.com>
2021-03-04 00:01:37 +0530

    enum changed to constant, to accept string to input

---
+++ projects/avataaars-angular/src/lib/models/constants/skinColor.constant.ts
@@ -0,0 +1,9 @@
+ export const SKIN_COLOR:{ [index:string] : string }={
+    Tanned :'#FD9841',
+    Light :'#EDB98A',
+    Yellow : '#F8D25C',
+    Pale :'#FFDBB4',
+    Brown : '#D08B5B',
+    DarkBrown : '#AE5D29',
+    Black : '#614335'
+ }
